6181eda181ef16a1c1cf3591e7402564500e21b8
kamolkar <50257304+kamolkar@users.noreply.github.com>
2026-06-03 08:51:35 -0500

    Add files via upload

---
+++ test.config.ts
@@ -0,0 +1,16 @@
+export class TestConfig{
+
+    ACT2QA_SANDBOXURL="https://readiness--act2qa.sandbox.lightning.force.com"
+    ACT2QA_USERNAME="s.sudha@eadiness.salesforce.com.act2qa"
+    ACT2QA_PASSWORD="******"
+    ACT2QA_SANDBOXADMINURL="https://readiness--act2qa.sandbox.my.salesforce.com"
+    ACT2QA_ADMINUSERNAME="s.sudha@salesforce.com"
+    ACT2QA_ADMINPASSWORD="*******"
+    AppACTLearningAdmin="ACT Learning Admin"
+    CBAssetType="Content Block"
+    EventAssetType="Event"
+  
+    
+    
+
+}
